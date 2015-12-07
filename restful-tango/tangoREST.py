d1968a1e5050b18827966de341e9c875a46b1073
Yashas Kumar <yrkumarr@gmail.com>
2015-12-07 15:22:20 -0500

    Remove module structure

--- restful-tango/tangoREST.py
+++ restful-tango/tangoREST.py
@@ -11,6 +11,11 @@ import hashlib
 import json
 import logging
 
+currentdir = os.path.dirname(
+        os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir)
+
 from tango import TangoServer
 from tangoObjects import TangoJob, TangoMachine, InputFile
 
