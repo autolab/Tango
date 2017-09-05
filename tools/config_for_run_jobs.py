02964602a9c56a019ffd02db6c1dbad762d99acd
Xiaolin Charlene Zang <czang@cmu.edu>
2017-09-05 18:05:24 +0000

    Add a separate config file for run_jobs to separate config settings from logic changes.

---
+++ tools/config_for_run_jobs.py
@@ -0,0 +1,36 @@
+# This is a config file for run_jobs.py.
+# Change the file to fit your environment.
+# Please do NOT commit your changes unless
+# 1. There is a need for more configuration settings and
+# 2. You have made it known to Xiaolin Charlene Zang.
+
+class Config:
+  # The settings are listed in the order of most-likly a changed is needed
+  # to the least-likely.
+
+  # YOUR course name
+  course = "czang-exp"
+  
+  # YOUR root dir for course/lab definitions and handin
+  courseRoot = "/n/scratch/czang/f16/"
+
+  # YOUR lab definitions. The index of the lab is given to run_job.py
+  labs = [
+    {"name": "myftlcheckpoint1", "handinSuffix": ".cpp", "image": "746.img"},
+    {"name": "myftlcheckpoint3", "handinSuffix": ".cpp", "image": "newPool.img"},
+    {"name": "cloudfscheckpoint1fuse", "handinSuffix": ".tar", "image": "newPool.img"}]
+
+  # YOUR Tango container's root dir for submissions and output
+  tangoFileRoot = "/mnt/charlene/tango_courselabs"
+  
+  # YOUR Tango repo root (cloned from Autolab github)
+  tangoDir = "/mnt/charlene/Tango"
+
+  # Sometimes multiple experimental Tango containers are run on one machine.
+  # They are identified by different ports.
+  tangoHostPort = "host-port 8600"
+
+  # IP of the tango container is usually computed automatically
+  tangoIP = ""
+  
+# end of class Config
