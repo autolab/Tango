autolab/Tango

commit 05e4d0f375afccd35b85ebd5ef8b7f96000f6324

Use tornado streaming for uploads (#154)
* use tornado streaming for uploads

use tornado.web.stream_request_body for the upload handler to avoid
reading the entire upload into a memory buffer *in the IOLoop*. Instead,
hash the content as it's streamed in and store it in a temporary file under
COURSELABS. Then the upload business logic can just rename the temporary file
over the destination if the upload is accepted. This significantly improves
latency when large files are uploaded

* Remove tempfile if it's a duplicate

Remove the temporary upload file if it's a duplicate instead of leaving it
in the temp directory.

restful-tango/server.py
@@ -1,6 +1,9 @@
 import tornado.web
 import urllib
 import sys
+import os
+from tempfile import NamedTemporaryFile
+import hashlib
 from concurrent.futures import ThreadPoolExecutor
 from functools import partial, wraps
 
@@ -57,18 +60,35 @@ class OpenHandler(tornado.web.RequestHandler):
         """ get - Handles the get request to open."""
         return tangoREST.open(key, courselab)
 
-
+@tornado.web.stream_request_body
 class UploadHandler(tornado.web.RequestHandler):
 
+    def prepare(self):
+        """ set up the temporary file"""
+        tempdir="%s/tmp" % (Config.COURSELABS,)
+        if not os.path.exists(tempdir):
+           os.mkdir(tempdir, 0700)
+        if os.path.exists(tempdir) and not os.path.isdir(tempdir):
+           tangoREST.log("Cannot process uploads, %s is not a directory" % (tempdir,))
+           return self.send_error()
+        self.tempfile = NamedTemporaryFile(prefix='upload', dir=tempdir,
+                                           delete=False)
+        self.hasher = hashlib.md5()
+
+    def data_received(self, chunk):
+        self.hasher.update(chunk)
+        self.tempfile.write(chunk)
+        
     @unblock
     def post(self, key, courselab):
         """ post - Handles the post request to upload."""
+        name = self.tempfile.name
+        self.tempfile.close
         return tangoREST.upload(
             key,
             courselab,
             self.request.headers['Filename'],
-            self.request.body)
-
+            name, self.hasher.hexdigest())
 
 class AddJobHandler(tornado.web.RequestHandler):
 

restful-tango/tangoREST.py
@@ -261,7 +261,7 @@ class TangoREST:
             self.log.info("Key not recognized: %s" % key)
             return self.status.wrong_key
 
-    def upload(self, key, courselab, file, body):
+    def upload(self, key, courselab, file, tempfile, fileMD5):
         """ upload - Upload file as an input file in key-courselab if the
         same file doesn't exist already
         """
@@ -271,15 +271,13 @@ class TangoREST:
             labPath = self.getDirPath(key, courselab)
             try:
                 if os.path.exists(labPath):
-                    fileMD5 = hashlib.md5(body).hexdigest()
                     if self.checkFileExists(labPath, file, fileMD5):
                         self.log.info(
                             "File (%s, %s, %s) exists" % (key, courselab, file))
+                        os.unlink(tempfile)
                         return self.status.file_exists
                     absPath = "%s/%s" % (labPath, file)
-                    fh = open(absPath, "wt")
-                    fh.write(body)
-                    fh.close()
+                    os.rename(tempfile, absPath)
                     self.log.info(
                         "Uploaded file to (%s, %s, %s)" %
                         (key, courselab, file))
@@ -287,12 +285,15 @@ class TangoREST:
                 else:
                     self.log.info(
                         "Courselab for (%s, %s) not found" % (key, courselab))
+                    os.unlink(tempfile)
                     return self.status.wrong_courselab
             except Exception as e:
                 self.log.error("upload request failed: %s" % str(e))
+                os.unlink(tempfile)
                 return self.status.create(-1, str(e))
         else:
             self.log.info("Key not recognized: %s" % key)
+            os.unlink(tempfile)
             return self.status.wrong_key
 
     def addJob(self, key, courselab, jobStr):

commit 9bf7ac02826c6d63cd61200963cb21c162e3acf4

Fix regression during file upload.
Regression caused by commit 05e4d0f375afccd35b85ebd5ef8b7f96000f6324. Files are
copied from a temp file to the final destination as the file has not been fully
uploaded and flushed. That yielded to grading corrupted or empty files.

restful-tango/server.py
@@ -83,7 +83,7 @@ class UploadHandler(tornado.web.RequestHandler):
     def post(self, key, courselab):
         """ post - Handles the post request to upload."""
         name = self.tempfile.name
-        self.tempfile.close
+        self.tempfile.close()
         return tangoREST.upload(
             key,
             courselab,