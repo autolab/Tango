6b6de4c7924c5b4f9c68c8720113d0fc7057c8c7
Xiaolin Charlene Zang <charlene@xzang.net>
2019-08-14 15:06:24 -0400

    Fix check_jobs (cron job in production Autolab): comment its purpose, report file writing failure, always write current trouble jobs into file

--- tools/check_jobs.py
+++ tools/check_jobs.py
@@ -9,27 +9,35 @@ from util import CommandLine
 from util import Lab
 import util
 
-# Drive exiting student submissions to Tango.
-# Find course/lab at specified location and submits work from the handin directory.
-# Then wait for job output files.
-#
-# Use -h to show usage.
-# See config_for_run_jobs.py for configuration options.
+# This script is run as a cron job every minute to detect potentially
+# stuck jobs and send email to the administrator.
+# It asks Tango for the live jobs.  Then it looks at the last-seen
+# timestamp in each job's trace to determine if it's a "slow" job.
+# It keeps the questionable jobs in a file so that they are not
+# reported again by the next execution of this script.
+# Potential false negative:  Suppose Tango dies and is restarted,
+# then the jobIds stored in the "reported jobs" file from Tango's last
+# incarnation may overlap with the current jobIds.  In such case,
+# the overlapping jobIds will not be reported.  However, when Tango
+# is stuck there usually will be more stuck jobs to be reported for
+# the admin's attention.
 
 cfg = Config()
 cmd = Cmd(cfg, None)
 
-REPORTED_JOBS_PATH = "/var/run/tango/check_jobs.json"
+REPORTED_JOBS_PATH = "/var/log/tango/lastSeenSlowJobsBy_check_jobs"
 
 jsonResult = {}
-reportedJobs = []
+reportedJobs = []  # trouble jobs found in last execution
+troubleJobs = []  # trouble jobs found in this execution
+writeFailure = ""
 mailbodyP1 = ""
-mailbodyP2 = "\nDetails:\n"
+mailbodyP2 = ""
 
 def sendmail():
     global mailbodyP1, mailbodyP2
 
-    if not mailbodyP1:
+    if not mailbodyP1 and not writeFailure:
         print "No error to report @ %s" % datetime.datetime.now()
         return
 
@@ -43,7 +51,7 @@ def sendmail():
                 "To: %s" % TO,
                 "Subject: %s" % SUBJECT ,
                 "",
-                mailbodyP1 + mailbodyP2
+                writeFailure + mailbodyP1 + mailbodyP2
                 ), "\r\n")
     server = smtplib.SMTP(HOST)
     server.sendmail(FROM, ["czang@cmu.edu", "jboles@cmu.edu"], BODY)
@@ -53,17 +61,22 @@ def sendmail():
 def report(jobId, msg):
     global mailbodyP1, mailbodyP2
     email = ""
-    
+
+    # add into trouble list but may not report this time
+    troubleJobs.append(jobId)
     if jobId in reportedJobs:
         return
 
-    reportedJobs.append(jobId)
-    for job in jsonResult["jobs"]:    
-        if job["id"] == jobId:
-            mailbodyP2 += json.dumps(job, indent=2, sort_keys=True)
-            matchObj = re.match(r'(.*)_[0-9]+_(.*)', job["name"], re.M|re.I)
-            email = matchObj.group(2)
-    mailbodyP1 += "job " + str(jobId) + ", student " + email + ": "  + msg + "\n"
+    # go through the job list to find the job by jobId
+    for job in jsonResult["jobs"]:
+        if job["id"] != jobId: continue
+        if not mailbodyP1:
+            mailbodyP1 = "\nTrouble jobs:\n"
+            mailbodyP2 = "\nJob details:\n"
+        matchObj = re.match(r'(.*)_[0-9]+_(.*)', job["name"], re.M|re.I)
+        email = matchObj.group(2)
+        mailbodyP1 += "job %s, student %s: %s\n" % (jobId, email, msg)
+        mailbodyP2 += json.dumps(job, indent=2, sort_keys=True)
 
 # use a dump file for testing
 if 0:
@@ -78,7 +91,7 @@ except:
     reportedJobs = []
     
 jsonResult = cmd.returnLiveJobs()  # comment out this line to use test data
-    
+
 for job in jsonResult["jobs"]:
     jobId = job["id"]
     if "trace" not in job:
@@ -100,9 +113,15 @@ for job in jsonResult["jobs"]:
         report(jobId, "It's been too long since last trace")
 # end of for loop
 
+# write troubled jobs found in this execution to file
+try:
+    with open(REPORTED_JOBS_PATH, 'w') as outfile:
+        json.dump(troubleJobs, outfile)
+except Exception as e:
+    writeFailure = "Failed to write to %s: %s\n" % (REPORTED_JOBS_PATH, e)
+
+# report trouble jobs AND maybe failure of writing to file
 sendmail()
-with open(REPORTED_JOBS_PATH, 'w') as outfile:
-    json.dump(reportedJobs, outfile)
 
 exit()
 
