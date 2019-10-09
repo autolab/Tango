ad177f6e2e3d1e39f0c35f95d57c1e056d5f58bd
Zeng Fan Pu <zengfanpu@gmail.com>
2019-10-09 09:50:29 -0400

    Fix start script

--- start/start-docker-dev.sh
+++ start/start-docker-dev.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-dockerd &
+# docker pull autolabproject/autograding_image
 docker pull autolabproject/autograding_image
 python restful-tango/server.py 3001 &
 python jobManager.py
