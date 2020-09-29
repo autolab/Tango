2434b32f51d356f9c16d2984c7ab54b8d14e45b5
Fan Pu <zengfanpu@gmail.com>
2020-09-29 16:24:44 -0400

    Fix env vars (no quotes)

--- setup.sh
+++ setup.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
-# replace the Devise secret key with a random string
-sed -i"" "s/<YOUR-SECRET-KEY>/`LC_ALL=C tr -dc 'A-Za-z0-9' </dev/urandom | head -c 128`/g" Autolab/docker/devise.rb
+echo "Creating default Autolab/config/school.yml"
+cp -n ./Autolab/config/school.yml.template ./Autolab/config/school.yml
+
+echo "Creating default Autolab/config/initializers/devise.rb"
+cp -n ./Autolab/config/initializers/devise.rb.template ./Autolab/config/initializers/devise.rb
+
+# Replace the Devise secret key with a random string
+echo "Setting random Devise secret"
+sed -i"" "s/<YOUR-SECRET-KEY>/`LC_ALL=C tr -dc 'A-Za-z0-9' </dev/urandom | head -c 128`/g" ./Autolab/config/initializers/devise.rb
+
+echo "Creating default Autolab/config/environments/production.rb"
+cp -n ./Autolab/config/environments/production.rb.template ./Autolab/config/environments/production.rb
+
+echo "Creating default Autolab/config/autogradeConfig.rb"
+cp -n ./Autolab/config/autogradeConfig.rb.template ./Autolab/config/autogradeConfig.rb
