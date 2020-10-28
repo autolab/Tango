14f017bc766f87987b0c2c904718c36b2bc829d5
Fan Pu <zengfanpu@gmail.com>
2020-10-28 21:15:54 +0000

    working configs

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -9,7 +9,7 @@ domains=(test.autolab.io)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="zengfanpu@gmail.com" # Adding a valid address is strongly recommended
-staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
