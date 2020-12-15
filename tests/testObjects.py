dda21255e42607c45f86e527a2768a06fa65a55f
mojojojo <statixe@gmail.com>
2020-12-15 14:30:55 -0500

    added tests for TangoQueue object

--- tests/testObjects.py
+++ tests/testObjects.py
@@ -4,11 +4,11 @@ from builtins import str
 import unittest
 import redis
 
-from tangoObjects import TangoDictionary, TangoJob
+from tangoObjects import TangoDictionary, TangoJob, TangoQueue
 from config import Config
 
 
-class TestObjects(unittest.TestCase):
+class TestDictionary(unittest.TestCase):
 
     def setUp(self):
         if Config.USE_REDIS:
@@ -55,6 +55,71 @@ class TestObjects(unittest.TestCase):
     def test_remoteDictionary(self):
         Config.USE_REDIS = True
         self.runDictionaryTests()
+
+
+class TestQueue(unittest.TestCase):
+    def setUp(self):
+        if Config.USE_REDIS:
+            __db = redis.StrictRedis(
+                Config.REDIS_HOSTNAME, Config.REDIS_PORT, db=0)
+            __db.flushall()
+        self.test_entries = [i for i in range(10)]
+    
+    def addAllToQueue(self):
+        # Add all items into the queue
+        for x in self.test_entries:
+            self.testQueue.put(x)
+            self.expectedSize += 1
+            self.assertEqual(self.testQueue.qsize(), self.expectedSize)
+
+    def runQueueTests(self):
+        self.testQueue = TangoQueue("self.testQueue")
+        self.expectedSize = 0
+        self.assertEqual(self.testQueue.qsize(), self.expectedSize)
+        self.assertTrue(self.testQueue.empty())
+
+        self.addAllToQueue()
+
+        # Test the blocking get
+        for x in self.test_entries:
+            item = self.testQueue.get()
+            self.expectedSize -= 1
+            self.assertEqual(self.testQueue.qsize(), self.expectedSize)
+            self.assertEqual(item, x)
+
+        self.addAllToQueue()
+
+        # Test the blocking get
+        for x in self.test_entries:
+            item = self.testQueue.get_nowait()
+            self.expectedSize -= 1
+            self.assertEqual(self.testQueue.qsize(), self.expectedSize)
+            self.assertEqual(item, x)
+
+        self.addAllToQueue()
+
+        # Remove all the even entries
+        for x in self.test_entries:
+            if (x % 2 == 0):
+                self.testQueue.remove(x)
+                self.expectedSize -= 1
+                self.assertEqual(self.testQueue.qsize(), self.expectedSize)
+
+        # Test that get only returns odd keys in order
+        for x in self.test_entries:
+            if (x % 2 == 1):
+                item = self.testQueue.get_nowait()
+                self.expectedSize -= 1
+                self.assertEqual(self.testQueue.qsize(), self.expectedSize)
+                self.assertEqual(item, x)
+
+    def test_nativeQueue(self):
+        Config.USE_REDIS = False
+        self.runQueueTests()
+
+    def test_remoteQueue(self):
+        Config.USE_REDIS = True
+        self.runQueueTests()
 
 
 if __name__ == '__main__':
